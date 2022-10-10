4c03e58a6c395b8462eb1648837ad353fb8ff1d4
supertren <supertren@hotmail.com>
2022-10-10 14:38:39 +0200

    comiteado Mon Oct 10 14:38:39 CEST 2022

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-2"
+  default = "us-east-1"
 }
 
 variable "PRIVATE_KEY_PATH" {
@@ -17,7 +17,7 @@ variable "AMI" {
   type = "map"
 
   default {
-    eu-west-2 = "ami-03dea29b0216a1e03"
+    us-east-1 = "ami-03dea29b0216a1e03"
     us-east-1 = "ami-0c2a1acae6667e438"
   }
 }
